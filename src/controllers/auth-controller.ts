4ac290ca2f7a88a08247f1c4e1a972ba872ba6c6
Filipe Garrote <filipe.garrote@gmail.com>
2022-11-09 13:18:56 -0300

    feat: Implementada rota de cadastro de usuário, com validação por joi e encriptação do password

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,16 +1,34 @@
 import { Request, Response } from 'express';
 import { SignUp } from '../protocols/auth-protocol';
+import bcrypt from 'bcrypt';
+import { signUpSchema } from '../schemas/authSchema.js';
+import { connection } from '../database/db.js';
 
-async function signUp(req: Request, res: Response){
-  const {name, email, password} = req.body as SignUp;
+async function signUp(req: Request, res: Response) {
+	const { name, email, password } = req.body as SignUp;
+	const { error } = signUpSchema.validate(req.body, { abortEarly: false });
 
+	if (error) {
+		const messages = error.details.map((err) => err.message).join('\n');
+		return res
+			.status(422)
+			.send(`Os seguintes erros foram encontrados:\n\n${messages}`);
+	}
 
+	const hashedPassword: string = await bcrypt.hash(password, 10);
+
+	try {
+		await connection.query(
+			'INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',
+			[name, email, hashedPassword]
+		);
+
+		res.sendStatus(201);
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
 }
 
+async function signIn(req: Request, res: Response) {}
 
-async function signIn(req: Request, res: Response){
-
-
-}
-
-export {signIn, signUp}
+export { signIn, signUp };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRouter from './routers/authRouter.js';
 dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.use(authRouter);
 
 server.listen(process.env.PORT, () =>
 	console.log(`Listening on port ${process.env.PORT}`)
